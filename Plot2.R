8b547f2a9f2fb5546fca0e2845d0f30fac10461f
Phil Renner <rennerp@comcast.net>
2021-01-24 22:16:42 -0800

    Plots 1-3 initial commit

---
+++ Plot2.R
@@ -0,0 +1,29 @@
+
+# get the source file, download as emission.zip
+if (!file.exists("emission.zip")){
+     fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    download.file(fileURL, "emission.zip", method="curl")
+}
+
+#unzip into emission dataset
+if (!file.exists("summarySCC_PM25.rds")) { 
+  unzip("emission.zip") 
+}
+
+#read into data frames emission and SCC (source classification codes)
+emission <-  readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+Balto <- emission[which(emission$fips == "24510"),]
+
+#create new variable of newtime
+#power1$newtime <- paste(power1$Date, power1$Time)
+#power1$newtime <- strptime(power1$newtime, format ="%m/%d/%Y %H:%M:%S")
+
+#create histogram of Total PM25 Emissions as Plot1.png
+png(filename = "Plot2.png", width = 480, height = 480, units = "px")
+barplot(tapply(Balto$Emissions, Balto$year, FUN=sum), main = "Total PM25 Emissions, Baltimore")
+dev.off()
+
+
+
---
+++ Plot3.R
@@ -0,0 +1,37 @@
+
+library(ggplot2)
+
+# get the source file, download as emission.zip
+if (!file.exists("emission.zip")){
+     fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    download.file(fileURL, "emission.zip", method="curl")
+}
+
+#unzip into emission dataset
+if (!file.exists("summarySCC_PM25.rds")) { 
+  unzip("emission.zip") 
+}
+
+#read into data frames emission and SCC (source classification codes)
+emission <-  readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+
+#create summary table, summarizing emissions by year and type, for Baltimore
+Balto <- emission[which(emission$fips == "24510"),]
+
+Baltosum <- Balto %>% 
+       group_by(year, type) %>% 
+       summarise(Emissions = mean(Emissions))
+Baltosum$year2 <- as.factor(Baltosum$year)
+
+#create plot of Total PM25 Emissions as Plot1.png
+png(filename = "Plot3.png", width = 480, height = 480, units = "px")
+g <- ggplot(Baltosum, aes(x= year2, y = Emissions))
+g + geom_col() + facet_grid(. ~ type)
+
+
+dev.off()
+
+
+
